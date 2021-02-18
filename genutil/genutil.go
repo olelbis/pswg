8e70adeda051ad2181f3471ef5c7f929a89d1640
Marco Gianini <marco.gianini.it@gmail.com>
2021-02-18 10:37:11 +0100

    Move constant grom main to genutil

--- genutil/genutil.go
+++ genutil/genutil.go
@@ -5,6 +5,15 @@ import (
 	"time"
 )
 
+//NN numeric string
+const NN string = "1234567890"
+
+//LS alphanumberic string
+const LS string = "abcdefghijklmnopqrstuvwxyz"
+
+//SS special character string
+const SS string = "!&%$£=?ù^+*][{}-_.:,;()><"
+
 //Melee : Get a string in input e do some shuffle
 func Melee(pwdin string) string {
 	rand.Seed(time.Now().Unix())
